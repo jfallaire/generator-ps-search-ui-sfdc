027a51d6a75af9a4cbeb0f66123d5fd5216912fc
COVEO\lgcarrier <lgcarrier@coveo.com>
2017-08-21 10:56:11 -0400

    Sass refactoring
    + add AgentBoxCaseCreation.scss
    * modify gulp task (issue with sass include path + handle agent box case creation)

--- generators/app/templates/gulpTasks/css.js
+++ generators/app/templates/gulpTasks/css.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const event_stream = require('event-stream');
 
-gulp.task('css', ['build-communityFullSearchCss', 'build-agentBoxCss', 'build-agentFullCss']);
+gulp.task('css', ['build-communityFullSearchCss', 'build-agentBoxCss', 'build-agentBoxCaseCreationCss', 'build-agentFullCss']);
 
 gulp.task('prepareSass', function () {
   return event_stream.merge(
@@ -14,21 +14,28 @@ gulp.task('prepareSass', function () {
 
 gulp.task('build-communityFullSearchCss', function (done) {
   return gulp.src('./sass/CommunityFullSearch.scss')
-    .pipe(sass({ includePaths: ['./sass/modules'] }))
+    .pipe(sass({ includePaths: ['./sass/ui'] }))
     .pipe(rename('CommunityFullSearch.<%= capitalizeCustomerSafeName %>.css'))
     .pipe(gulp.dest('./bin/css'));
 });
 
 gulp.task('build-agentBoxCss', function (done) {
   return gulp.src('./sass/AgentBox.scss')
-    .pipe(sass({ includePaths: ['./sass/modules'] }))
+    .pipe(sass({ includePaths: ['./sass/ui'] }))
     .pipe(rename('AgentBox.<%= capitalizeCustomerSafeName %>.css'))
+    .pipe(gulp.dest('./bin/css'));
+});
+
+gulp.task('build-agentBoxCaseCreationCss', function (done) {
+  return gulp.src('./sass/AgentBoxCaseCreation.scss')
+    .pipe(sass({ includePaths: ['./sass/ui'] }))
+    .pipe(rename('AgentBoxCaseCreation.<%= capitalizeCustomerSafeName %>.css'))
     .pipe(gulp.dest('./bin/css'));
 });
 
 gulp.task('build-agentFullCss', function (done) {
   return gulp.src('./sass/AgentFullSearch.scss')
-    .pipe(sass({ includePaths: ['./sass/modules'] }))
+    .pipe(sass({ includePaths: ['./sass/ui'] }))
     .pipe(rename('AgentFullSearch.<%= capitalizeCustomerSafeName %>.css'))
     .pipe(gulp.dest('./bin/css'));
 });
