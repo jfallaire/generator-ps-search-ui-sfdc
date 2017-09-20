c72086f7fdd4ab51d3e31cde94ed9a064017b6ce
COVEO\lgcarrier <lgcarrier@coveo.com>
2017-09-20 10:54:52 -0400

    update css gulp task to be more flexible and dynamic
    * update css gulp task

--- generators/app/templates/gulpTasks/css.js
+++ generators/app/templates/gulpTasks/css.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const event_stream = require('event-stream');
 
-gulp.task('css', ['build-communityFullSearchCss', 'build-agentBoxCss', 'build-agentBoxCaseCreationCss', 'build-agentFullCss']);
+gulp.task('css', ['build-css']);
 
 gulp.task('prepareSass', function () {
   return event_stream.merge(
@@ -12,30 +12,11 @@ gulp.task('prepareSass', function () {
     ).pipe(event_stream.wait())
 });
 
-gulp.task('build-communityFullSearchCss', function (done) {
-  return gulp.src('./sass/CommunityFullSearch.scss')
-    .pipe(sass({ includePaths: ['./sass/ui'] }))
-    .pipe(rename('CommunityFullSearch.<%= capitalizeCustomerSafeName %>.css'))
-    .pipe(gulp.dest('./bin/css'));
-});
-
-gulp.task('build-agentBoxCss', function (done) {
-  return gulp.src('./sass/AgentBox.scss')
-    .pipe(sass({ includePaths: ['./sass/ui'] }))
-    .pipe(rename('AgentBox.<%= capitalizeCustomerSafeName %>.css'))
-    .pipe(gulp.dest('./bin/css'));
-});
-
-gulp.task('build-agentBoxCaseCreationCss', function (done) {
-  return gulp.src('./sass/AgentBoxCaseCreation.scss')
-    .pipe(sass({ includePaths: ['./sass/ui'] }))
-    .pipe(rename('AgentBoxCaseCreation.<%= capitalizeCustomerSafeName %>.css'))
-    .pipe(gulp.dest('./bin/css'));
-});
-
-gulp.task('build-agentFullCss', function (done) {
-  return gulp.src('./sass/AgentFullSearch.scss')
-    .pipe(sass({ includePaths: ['./sass/ui'] }))
-    .pipe(rename('AgentFullSearch.<%= capitalizeCustomerSafeName %>.css'))
-    .pipe(gulp.dest('./bin/css'));
+gulp.task('build-css', function (done){
+  return gulp.src(['./sass/*.scss'])
+  .pipe(sass({ includePaths: ['./sass/ui'] }))
+  .pipe(rename(function (path){
+    path.basename += '.<%= capitalizeCustomerSafeName %>';
+  }))
+  .pipe(gulp.dest('./bin/css'));
 });
