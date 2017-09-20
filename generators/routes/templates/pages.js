ce15de06b23c06440289a97a9e6f830ecec943c3
COVEO\lgcarrier <lgcarrier@coveo.com>
2017-09-20 10:46:40 -0400

    default search interfaces
    + add pilot-search
    * update community-search
    - remove agent-box-case-creation
    *update routes

--- generators/routes/templates/pages.js
+++ generators/routes/templates/pages.js
@@ -3,14 +3,23 @@ var express = require('express');
 var router = express.Router();
 var cfg = require('../config');
 
-// Search page
-router.get('/community-search', function(req, res) {
-    res.render('pages/community-search', { 
-        prototypeTitle : 'Community Search Prototype',
+// Pilot Search
+router.get('/pilot-search', function(req, res) {
+    res.render('pages/pilot-search', { 
+        prototypeTitle : 'Pilot Search',
         config: cfg 
     });
 });
 
+// Community Search
+router.get('/community-search', function(req, res) {
+    res.render('pages/community-search', { 
+        prototypeTitle : 'Community Search',
+        config: cfg 
+    });
+});
+
+// SFDC Agent Box
 router.get('/agent-box', function(req, res) {
     res.render('pages/agent-box', { 
         prototypeTitle : 'Agent Insight Panel',
@@ -18,6 +27,7 @@ router.get('/agent-box', function(req, res) {
     });
 });
 
+// SFDC Agent Full
 router.get('/agent-full-search', function(req, res) {
     res.render('pages/agent-full-search', { 
         prototypeTitle : 'Agent Full Search',
